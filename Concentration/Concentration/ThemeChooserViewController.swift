450eee34b232babbbcaf27d6eb6c6d922ca18021
Julia <jmishkovets@gmail.com>
2018-04-21 17:22:31 +0200

    Start with theme chooser on iPhone if theme is nil

--- Concentration/Concentration/ThemeChooserViewController.swift
+++ Concentration/Concentration/ThemeChooserViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ThemeChooserViewController: UIViewController {
+class ThemeChooserViewController: UIViewController, UISplitViewControllerDelegate {
     
     private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
         return splitViewController?.viewControllers.last as? ConcentrationViewController
     }
+    
+    private var lastSequedToConcentrationViewController: ConcentrationViewController?
     
     let themes = [
         "Sports" : "🏋️‍♂️🥊⚽️🏀🏈⚾️🎾🏐⛳️🏉",
@@ -23,11 +25,16 @@ class ThemeChooserViewController: UIViewController {
     
     @IBAction func changeTheme(_ sender: Any) {
         guard let button = sender as? UIButton else { return }
-        
+    
         if let concentrationViewConroller = splitViewDetailConcentrationViewController {
             if let themeName = button.currentTitle, let theme = themes[themeName] {
                 concentrationViewConroller.theme = theme
             }
+        } else if let concentrationViewConroller = lastSequedToConcentrationViewController {
+            if let themeName = button.currentTitle, let theme = themes[themeName] {
+                concentrationViewConroller.theme = theme
+            }
+            navigationController?.pushViewController(concentrationViewConroller, animated: true)
         } else {
             performSegue(withIdentifier: "Choose Theme", sender: sender)
         }
@@ -40,9 +47,14 @@ class ThemeChooserViewController: UIViewController {
             if let themeName = button.currentTitle, let theme = themes[themeName] {
                 if let concentrationViewController = segue.destination as? ConcentrationViewController {
                     concentrationViewController.theme = theme
+                    lastSequedToConcentrationViewController = concentrationViewController
                 }
             }
         }
+    }
+    
+    override func awakeFromNib() {
+        splitViewController?.delegate = self
     }
     
     func splitViewController(_ splitViewController: UISplitViewController,
