3ac0d2942f809f580f1b1e53d8651f3132fea5cc
Julia <jmishkovets@gmail.com>
2018-04-21 16:59:52 +0200

    Don't reset a game completly when changing theme

--- Concentration/Concentration/ThemeChooserViewController.swift
+++ Concentration/Concentration/ThemeChooserViewController.swift
@@ -10,13 +10,29 @@ import UIKit
 
 class ThemeChooserViewController: UIViewController {
     
+    private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
+        return splitViewController?.viewControllers.last as? ConcentrationViewController
+    }
+    
     let themes = [
         "Sports" : "🏋️‍♂️🥊⚽️🏀🏈⚾️🎾🏐⛳️🏉",
         "Animals" : "🐶🦊🙈🐴🐌🐤🐼🐹🐭🐯",
         "Faces" : "👳‍♀️💆‍♀️🙅‍♂️👷‍♂️👨‍👧👶😂😅😉😏",
         "Halloween" : "👻🎃🍎🍭🙀🙈👹👺👿🧣"
     ]
-
+    
+    @IBAction func changeTheme(_ sender: Any) {
+        guard let button = sender as? UIButton else { return }
+        
+        if let concentrationViewConroller = splitViewDetailConcentrationViewController {
+            if let themeName = button.currentTitle, let theme = themes[themeName] {
+                concentrationViewConroller.theme = theme
+            }
+        } else {
+            performSegue(withIdentifier: "Choose Theme", sender: sender)
+        }
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let button = sender as? UIButton else { return }
         
@@ -27,6 +43,17 @@ class ThemeChooserViewController: UIViewController {
                 }
             }
         }
+    }
+    
+    func splitViewController(_ splitViewController: UISplitViewController,
+                             collapseSecondary secondaryViewController: UIViewController,
+                             onto primaryViewController: UIViewController) -> Bool {
+        if let concentrationViewConroller = secondaryViewController as? ConcentrationViewController {
+            if concentrationViewConroller.theme != nil {
+                return false
+            }
+        }
+        return true
     }
 
 }
